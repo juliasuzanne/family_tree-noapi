49767a61cfde7c9675e7b0e8ccc48268088aef5d
Julia Grimes <julia.s.grimes@gmail.com>
2022-11-17 21:44:55 -0500

    Add create, new actions with views

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,9 +5,22 @@ class PhotosController < ApplicationController
   end
 
   def show
+    @photo = Photo.find(params[:id])
+    render template: "photos/show"
   end
 
   def new
+    @photo = Photo.new(
+      name: params[:photo][:name],
+      width: params[:photo][:width],
+      height: params[:photo][:width],
+    )
+
+    if @photo.save
+      redirect_to "/photos"
+    else
+      render json: { errors: @photo.errors.full_messages }, status: unprocessable_entity
+    end
   end
 
   def create

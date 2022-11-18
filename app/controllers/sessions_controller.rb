1a9f5aa1a185f6fa8cbc57d0f61a570f52a2aa74
Julia Grimes <julia.s.grimes@gmail.com>
2022-11-17 22:26:07 -0500

    Add routes, methods, and views to allow login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+    render template: "sessions/new" #show form to login
+  end
+
+  def create # called when "new" is submitted? allows login and sets user_id
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to "/"
+    else
+      redirect_to "/login"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to "/login"
+  end
+end
